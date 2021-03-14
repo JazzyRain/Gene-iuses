322f25b480cad30292ad892a4890cd357f3d51c2
Rain Zhu <16ryz2@queensu.ca>
2021-03-14 03:05:26 -0300

    Third commit

--- Alignment.R
+++ Alignment.R
@@ -16,20 +16,21 @@ BbGbkBLAST<-blastSequences(paste(BbGbk@sequence),as = 'data.frame',hitListSize =
 ##multiple alignment
 BbHitsDF<-data.frame(ID=BbGbkBLAST$Hit_accession,Seq=BbGbkBLAST$Hsp_hseq,stringsAsFactors = FALSE)
 
-
-##muscle+ alignemnt inspection
-BbHitSeqs<-read.GenBank(BbGbkBLAST$Hit_accession[1:3])
+##muscle+ alignemnt inspection+ generate distance matrix 
+BbHitSeqs<-read.GenBank(BbGbkBLAST$Hit_accession[1:20])
 BbHitsDNA<-sapply(BbHitsDF$Seq,strsplit,split="")## convert the object to one with seperate column for each basepair using strsplit()and sapply() to split the DA for each row
 names(BbHitsDNA)<-paste(1:nrow(BbHitsDF),BbHitsDF$ID,sep="_")## give each sequence unique names.
 BbHitsDNA<-as.DNAbin(BbHitsDNA)## convert it into a DNAbin object 
 BbAlign<-muscle(BbHitsDNA,quiet=F)## run muscle on DNAbin object, allowing alignment 
 checkAlignment(BbAlign,what=3)## to inspect alignments and determine Shannon indexes relatives to the sequence position, providing cues to diversity
+BbDM<-dist.dna(BbAlign, model="K81") #use distance model K80, this estimates a pairwise distance matrix from the sequence data
 
-#Distance Matreix 
-BbDM<-dist.dna(BbAlign, model="K80") #use distance model K80, this estimates a pairwise distance matrix from the sequence data
-BbDMmat<-as.matrix(BbDM)
 ## tree building 
-BbTree<-nj(BbDM)
-ggtree(BbTree,layout="rectangular") + geom_tiplab()
+BbTree<-nj(BbDM)#neighbour joining 
+ggtree(BbTree,layout="rectangular") + geom_tiplab()##generate the phylogenetic tree using ggtree(), arranged in rectangular form with tip labels 
 
-
+#problems: 
+###I got a stick, and I don't think it is suppose to be a stick. Does anyone know how to fix it?
+  ## this is the tree result i get from BLAST: 
+    #https://blast.ncbi.nlm.nih.gov/blast/treeview/treeView.cgi?request=page&blastRID=4V81BY0501R&queryID=sp|Q9ZZ64|&entrezLim=&ex=&exl=&exh=&ns=100
+###Also we are suppose to show the phylogenetic relationship of species, instead we are currently showing geneIDs, this should be fixed. 
