2145c158007117d3f5e5b4fcdfb5ab892abc14a0
Zach Lehman <zachlehman@zachs-macbook-pro-2.local>
2021-03-26 14:39:33 -0400

    changing the method how we do the alignment, grabbing the COX1 sequence directly from the dog's mt genome by clipping the unneeded sequences out. setting up the distance matrix and tree - i'll come back and edit the tree since i think it could be made to look better, and add comments to each line of code

--- Alignment.R
+++ Alignment.R
@@ -1,35 +1,41 @@
-install.packages("BiocManager")
 library(BiocManager)
-install(c("genbankr","Biostrings","ggtree","annotate")) 
-install.packages(c("reshape2","rentrez"))
 library(genbankr)
 library(annotate)
 library(ape)
 library(ggtree)
+library(rentrez)
 
-BbID<-GBAccession("NC_002008.4")#using NCBI I seached for CO1, and canis lupus familiaris [organism], i find this accession number
-BbGbk<-readGenBank(BbID)##use the accession number to browse through the gene bank 
+mtID<-GBAccession("NC_002008.4") #using NCBI I seached for CO1, and canis lupus familiaris [organism], i find this accession number
+mtRead<-readGenBank(BbID) #use the accession number to browse through the gene bank 
+mtRead@genes[3] #since COX1, our reference gene of interest, is 3rd on the list, we can index it to see the exact range of the gene. The IRange given is 5349-6893. So let's snip to be just this range!
+
+mt<-entrez_fetch(dbfrom="gene", id="NC_002008.4", db="nuccore", rettype="fasta")
+mtSeq<-gsub("^>.*genome\\n([ATCG].*)","\\1",mt)
+mtSeq<-gsub("\\n", "",mtSeq)
+
+for(i in 1:nchar(mtSeq)){
+  if(i<5349){
+    COX1<-sub("\\w", "", mtSeq)
+  } else if(i>6893){
+    COX1<-sub("\\w$", "", COX1)
+  }
+}
 
 ##Pairwise alignment
-BbGbkBLAST<-blastSequences(paste(BbGbk@sequence),as = 'data.frame',hitListSize = 40, timeout = 600)## pairwise alignment search using our gene 
+COX1BLAST<-blastSequences(COX1, as='data.frame', hitListSize=40, timeout=600) #pairwise alignment search using our gene 
 ##multiple alignment
-BbHitsDF<-data.frame(ID=BbGbkBLAST$Hit_accession,Seq=BbGbkBLAST$Hsp_hseq,stringsAsFactors = FALSE)
+COXHitsDF<-data.frame(ID=COX1BLAST$Hit_accession, Seq=COX1BLAST$Hsp_hseq, stringsAsFactors=FALSE)
 
-##muscle+ alignemnt inspection+ generate distance matrix 
-BbHitSeqs<-read.GenBank(BbGbkBLAST$Hit_accession[1:20])
-BbHitsDNA<-sapply(BbHitsDF$Seq,strsplit,split="")## convert the object to one with seperate column for each basepair using strsplit()and sapply() to split the DA for each row
-names(BbHitsDNA)<-paste(1:nrow(BbHitsDF),BbHitsDF$ID,sep="_")## give each sequence unique names.
-BbHitsDNA<-as.DNAbin(BbHitsDNA)## convert it into a DNAbin object 
-BbAlign<-muscle(BbHitsDNA,quiet=F)## run muscle on DNAbin object, allowing alignment 
-checkAlignment(BbAlign,what=3)## to inspect alignments and determine Shannon indexes relatives to the sequence position, providing cues to diversity
-BbDM<-dist.dna(BbAlign, model="K81") #use distance model K80, this estimates a pairwise distance matrix from the sequence data
 
-## tree building 
-BbTree<-nj(BbDM)#neighbour joining 
-ggtree(BbTree,layout="rectangular") + geom_tiplab()##generate the phylogenetic tree using ggtree(), arranged in rectangular form with tip labels 
+#muscle+ alignemnt inspection+ generate distance matrix 
+COXHitsSeqs<-read.GenBank(COX1BLAST$Hit_accession[1:40])
+COXHitsDNA<-sapply(COXHitsDF$Seq, strsplit, split="") #convert the object to one with seperate column for each basepair using strsplit()and sapply() to split the DA for each row
+names(COXHitsDNA)<-paste(1:nrow(COXHitsDF), COXHitsDF$ID, sep="_") #give each sequence unique names.
+COXHitsDNA<-as.DNAbin(COXHitsDNA) #convert it into a DNAbin object 
+COXAlign<-muscle(COXHitsDNA, quiet=F) #run muscle on DNAbin object, allowing alignment 
+checkAlignment(COXAlign, what=3) #to inspect alignments and determine Shannon indexes relatives to the sequence position, providing cues to diversity
+COXDM<-dist.dna(COXAlign, model="K81") #use distance model K80, this estimates a pairwise distance matrix from the sequence data
 
-#problems: 
-###I got a stick, and I don't think it is suppose to be a stick. Does anyone know how to fix it?
-  ## this is the tree result i get from BLAST: 
-    #https://blast.ncbi.nlm.nih.gov/blast/treeview/treeView.cgi?request=page&blastRID=4V81BY0501R&queryID=sp|Q9ZZ64|&entrezLim=&ex=&exl=&exh=&ns=100
-###Also we are suppose to show the phylogenetic relationship of species, instead we are currently showing geneIDs, this should be fixed. 
+# tree building 
+COXTree<-nj(COXDM) #neighbour joining 
+ggtree(COXTree, layout="rectangular")+geom_tiplab() #generate the phylogenetic tree using ggtree(), arranged in rectangular form with tip labels 
